265e857475d69b5d173da3f0269c56a07caa9ae0
Diego Ferreira <diegoferreira710@gmail.com>
2020-11-08 11:10:57 -0300

    Teste de Login e manter sessão com o Curl

--- tests/tests.php
+++ tests/tests.php
@@ -1,24 +1,5 @@
 <?php
 
-//passo 1: ter um login
-
-//passo 2: ter uma sessão válida
-
-//passo 3: enviar um form de transaction para o store.php
-
-//passo 4: verificar a resposta do teste
-
-//passo 5: desfazer do banco as alterações feitas
-
-//passo 6: deslogar
-
-require_once 'utils.php';
-
-runTest('testRegister');
-
-
-
-
 /**
  * Testa a criação de um novo usuário.
  */
@@ -35,101 +16,21 @@ function testRegister()
     existsInDatabase('usuarios', ['nome' => $nome]);
 }
 
+function testLogin()
+{
 
+    $nome = 'nomeTeste';
+    $senha = '123456';
+    $repitaSenha = '123456';
+    $url = 'http://localhost:8000/login/register.php';
+    $result = sendRequest($url, ['nome' => $nome, 'senha' => $senha, 'repitaSenha' => $repitaSenha]);
 
-
-/**
- * @param string $url
- * @param mixed $body
- * Dispara uma requisição via curl retornando os dados e o código http. Caso o resultado seja JSON, também converte e retorna os dados.
- */
-function sendRequest ($url, $body, $method = 'get') {
-    $encodedFields = http_build_query($body);
-    $ch = curl_init();
-    curl_setopt($ch,CURLOPT_URL, $url);
-    if($method == 'post') {
-        curl_setopt($ch,CURLOPT_POST, true);
-    }
-    curl_setopt($ch,CURLOPT_POSTFIELDS, $encodedFields);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
-    $result = curl_exec($ch);
-    $info = curl_getinfo($ch);
-    if(strpos($info['content_type'], 'application/json') !== false){
-        $data = json_decode($result, true);
-        $result = $data['body'];
-    }
-    return ['http_code' => $info['http_code'], 'body' => $result];
-}
-
-/**
- * Rotina para apagar as tabelas do banco de dados após os testes
- */
-function eraseDatabase () {
-    truncateTable('movimentos');
-    truncateTable('usuarios');
-}
-
-/**
- * @param string $tableName nome da tabela da qual se deseja apagar todos os registros.
- * Função que apaga todos os registros da tabela.
- */
-function truncateTable ($tableName) {
-    $pdo = dbConnect();
-    $sql = "DELETE from $tableName;";
-    $result = $pdo->exec($sql);
-}
-
-/**
- * Dado o nome da função-teste, executa a mesma e limpa o banco logo após.
- */
-function runTest($testName) {
-    $testName();
-    eraseDatabase();
-}
-
-
-/**
- * @param string $table nome da tabela.
- * @param mixed $data array chave-valor de coluna-valor para se verificar no banco.
- * Verifica se, na $table informada, existe um ou mais registros com as condições de coluna-valor em $data.
- * Caso negativo, exibe mensagem de erro.
- */
-function existsInDatabase ($table, $data) {
-    $pdo = dbConnect();
-
-    $where = ' WHERE';
-    $i = 0;
-    foreach ($data as $campo => $valor) {
-        if($i > 0) {
-            $where .= " and";
-        }
-        $where .= " $campo = :$campo";
-        $i++;
-    }
-    $where .= ";";
-
-    $sql = "SELECT * FROM $table $where";
-    $statement = $pdo->prepare($sql);
-
-    foreach ($data as $campo => $valor) {
-        $statement->bindParam(":$campo", $valor, PDO::PARAM_STR);
-    }
-
-    $result = $statement->execute();
-    $list = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-    if(count($list) < 1){
-        echo "\n x Erro no teste! Valor ".var_export($data)." não existe no banco de dados.\n";
-    }
-}
-
-/**
- * @param string $value1
- * @param string $value2
- * Verifica se value1 é igual a value2. Caso negativo, exibe mensagem de erro.
- */
-function assertEquals ($value1 , $value2) {
-    if($value1 !== $value2){
-        echo "\n x Erro no teste! Falha ao verificar que ".var_export($value1,true)." é igual a ".var_export($value2,true).".\n\n";
-    }
+    $nome = 'nomeTeste';
+    $senha = '123456';
+    $url = 'http://localhost:8000/login/login.php';
+    $result = sendRequest($url, ['nome' => $nome, 'senha' => $senha], true);
+    assertEquals($result['http_code'], 200);
+    $url = 'http://localhost:8000/transaction/index.php';
+    $result = sendRequest($url, []);
+    assertEquals($result['http_code'], 200);
 }
